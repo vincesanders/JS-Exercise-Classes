74522ac2a8a955bff83f6da1097903dbccf30861
Vincent Sanders <vincesanders@hotmail.com>
2019-11-14 07:40:37 -0600

    I completed the second task.

--- index.js
+++ index.js
@@ -74,7 +74,26 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    let range = this.tank * this.milesPerGallon;
+    if (distance > range) {
+      this.odometer = range;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    } else {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    }
+  }
 }
 
 /*
